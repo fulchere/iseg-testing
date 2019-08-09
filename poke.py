50c1d18b67b70873b3b0b82bf1c90e3459b78992
Ethan Fulcher <fulchere@msu.edu>
2019-08-09 15:10:51 -0400

    more testing

--- poke.py
+++ poke.py
@@ -1,30 +1,23 @@
 import requests
-r = requests.get('http://192.168.1.101/api/login/user/user')
-print(r.content)
-print(r.text)
-print(r.status_code)
+#r = requests.get('http://192.168.1.101/api/login/user/user')
+#print(r.content, r.text, r.status_code) ### HTTP doesn't work with this power supply
 
 import websocket
 from websocket import create_connection
 #ws = websocket.WebSocket()
-#ws.connect("ws://ics.iseg-hv.com:8080", http_proxy_host="proxy_host_name", http_proxy_port=3128)
+#ws.connect("ws://192.168.1.101:8080", http_proxy_host="proxy_host_name", http_proxy_port=3128)
 
-ws = create_connection("ws://ics.iseg-hv.com:8080")
+ws = create_connection("ws://192.168.1.101:8080")
 import json
 
-json_data = '''{
-	"i": "",
-	"t": "login",
-	"c": {
-		"l": "admin",
-		"p": "password",
-		"t": ""
-	},
-	"r": "websocket"
-}'''
-st = json.loads(json_data)
-print(st)
-ws.send(st)
+login = '''{"i":"","t":"login","c":{"l":"user","p":"user","t":""},"r":"websocket"}'''
+#st = json.loads(json_data)
+
+
+ws.send(login)
+
+
 res = ws.recv()
 print(res)
 ws.close()
+
